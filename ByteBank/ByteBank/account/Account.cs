9a210d0b9f25b0260a805a858812cbac4abda91e
Tarcio Elyakin Agra Diniz <tarciodiniz0@gmail.com>
2023-02-23 21:53:25 -0300

    Part 1

---
+++ ByteBank/ByteBank/account/Account.cs
@@ -0,0 +1,57 @@
+﻿namespace ByteBank.account;
+
+public class Account
+{
+    private readonly string _name;
+    private readonly int _cpf;
+    private string _profession;
+    private decimal _balance;
+    private bool _isClosed;
+
+    public Account(string name, int cpf, string profession)
+    {
+        this._name = name;
+        this._cpf = cpf;
+        this._profession = profession;
+    }
+
+    public string GetName() => _name;
+    public int GetCpf() => _cpf;
+    public string GetProfession() => _profession;
+    public decimal GetBalance() => _balance;
+
+    public void SetProfession(string newProfession)
+    {
+        this._profession = newProfession;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+        {
+            this._balance += amount;
+        }
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount <= this._balance)
+        {
+            this._balance -= amount;
+        }
+    }
+
+    public bool Transfer(Account destinationAccount, decimal amount)
+    {
+        if (amount > this._balance) return false;
+        this.Withdraw(amount);
+        destinationAccount.Deposit(amount);
+        return true;
+    }
+
+    public void CloseAccount()
+    {
+        this._isClosed = true;
+    }
+    
+}
---
+++ ByteBank/ByteBank/account/CurrentAccount.cs
@@ -0,0 +1,24 @@
+﻿namespace ByteBank.account;
+
+public class CurrentAccount: Account
+{
+    private int _agency;
+    private int _account;
+    
+    public CurrentAccount(string name, int cpf, string profession) : base(name, cpf, profession)
+    {
+        SetAgency();
+    }
+
+    private void SetAgency()
+    {
+        this._agency = 021;
+    }
+
+    private void SetAccount()
+    {
+        var random = new Random();
+        this._agency = random.Next(0, 9999999);
+    }
+
+}
