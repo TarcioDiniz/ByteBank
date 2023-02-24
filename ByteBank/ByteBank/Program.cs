9413ff5051466b875ed803e1d7af76a32e333510
Tarcio Elyakin Agra Diniz <tarciodiniz0@gmail.com>
2023-02-23 22:23:20 -0300

    parte 2 - update

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ByteBank.account;
+
+CurrentAccount account = new CurrentAccount("João", 123456789, "Desenvolvedor");
+Array accountDetails = account.GetAccountDetails();
+
+Console.WriteLine("Detalhes da conta:");
+Console.WriteLine($"Agência: {accountDetails.GetValue(0)}");
+Console.WriteLine($"Conta: {accountDetails.GetValue(1)}");
+Console.WriteLine($"Nome: {accountDetails.GetValue(2)}");
+Console.WriteLine($"CPF: {accountDetails.GetValue(3)}");
+Console.WriteLine($"Profissão: {accountDetails.GetValue(4)}");
+Console.WriteLine($"Saldo: {accountDetails.GetValue(5)}");
+
+
+
+CurrentAccount account2 = new CurrentAccount("João2", 123456788, "Desenvolvedor");
+string[] accountDetails2 = (string[]) account2.GetAccountDetails();
+
+Console.WriteLine("Detalhes da conta:");
+Console.WriteLine(string.Join(Environment.NewLine, accountDetails2));
--- ByteBank/ByteBank/account/CurrentAccount.cs
+++ ByteBank/ByteBank/account/CurrentAccount.cs
@@ -1,14 +1,18 @@
 ﻿namespace ByteBank.account;
 
-public class CurrentAccount: Account
+public class CurrentAccount : Account
 {
     private int _agency;
     private int _account;
-    
+
     public CurrentAccount(string name, int cpf, string profession) : base(name, cpf, profession)
     {
         SetAgency();
+        SetAccount();
     }
+
+    public int GetAgency() => _agency;
+    public int GetAccount() => _account;
 
     private void SetAgency()
     {
@@ -18,7 +22,21 @@ public class CurrentAccount: Account
     private void SetAccount()
     {
         var random = new Random();
-        this._agency = random.Next(0, 9999999);
+        this._account = random.Next(0, 9999999);
     }
 
+    public Array GetAccountDetails()
+    {
+        string[] accountDetails =
+        {
+            $"{GetAgency()}",
+            $"{GetAccount()}",
+            $"{GetName()}",
+            $"{GetCpf()}",
+            $"{GetProfession()}",
+            $"{GetBalance()}"
+        };
+
+        return accountDetails;
+    }
 }
